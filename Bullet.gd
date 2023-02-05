79769983ec408bf01f8e61f9c20046ff66f32997
Vincent <vincentipoteet@gmail.com>
2023-02-05 12:06:33 -0600

    added bullet trail and explosion

--- Bullet.gd
+++ Bullet.gd
@@ -17,8 +17,18 @@ func _process(delta):
 	if collision:
 		if collision.collider.has_method('take_damage'):
 			collision.collider.take_damage(damage)
+			
+		spawn_particles()
 		queue_free()
 
 
 func _on_VisibilityNotifier2D_screen_exited():
 	queue_free()
+
+
+func spawn_particles():
+	var particles = preload("res://Impact.tscn").instance()
+	particles.position = global_position
+	particles.emitting = true
+	particles.one_shot = true
+	get_tree().current_scene.add_child(particles)
